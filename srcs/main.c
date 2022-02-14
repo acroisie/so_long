2000fcd6549c62782430eba026c003b860fb21c9
acroisie <acroisier@student.42lyon.fr>
2022-02-14 13:35:41 +0100

    update_asset

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 13:55:24 by acroisie          #+#    #+#             */
-/*   Updated: 2022/02/10 15:47:12 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2022/02/14 13:14:48 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main(int argc, char **argv)
 
 	game.map = ft_error_check(argc, argv);
 	game.mlx = mlx_init();
-	game.mlx_window = mlx_new_window(game.mlx, 640, 480, "so_long");
+	game.mlx_window = mlx_new_window(game.mlx, 640, 480, "so_long"); // Calculate window size
 	mlx_hook(game.mlx_window, 17, 0L, ft_clean_exit, &game);
 	mlx_hook(game.mlx_window, 2, 1L << 0, ft_key_hook, &game);
 	mlx_loop(game.mlx);
